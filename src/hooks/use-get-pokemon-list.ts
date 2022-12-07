63302a141a7d00be6c5275b486943dce06e5347c
Alessio Sferro <sferro.alessio@gmail.com>
2022-12-07 08:48:43 +0100

    feat: add react query pokemon list

--- src/hooks/use-get-pokemon-list.ts
+++ src/hooks/use-get-pokemon-list.ts
@@ -1,5 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { PokeClient } from '../../pages/_app';
+import { pokemonListQuery } from '@/queries/pokemon-list';
 
 export const useGetPokemonList = () => {
-  return useQuery(['pokemon-list'], async () => {});
+  return useQuery(['pokemon-list'], async () => {
+    const { pokemon_v2_pokemon } = await PokeClient.request(pokemonListQuery);
+
+    return pokemon_v2_pokemon;
+  });
 };
